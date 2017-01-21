4586aadd3e64c651c769a026a189d80a026e8fba
Tom <tom@pactcoffee.com>
2017-01-21 17:51:35 +0000

    implement page url function
    - guard clause to make sure the script only runs when called from it’s
    file

---
+++ fetch_data.rb
@@ -0,0 +1,13 @@
+def page_url(page_number)
+  base_url = "http://www.wegottickets.com/searchresults/page/PAGE_NUMBER/all#paginate"
+  page_url = base_url.gsub(/PAGE_NUMBER/, page_number.to_s)
+end
+
+
+if $0 == __FILE__
+  (1..10).each do |page_number|
+    print "\nFetching results for page #{page_number}: "
+    url = page_url page_number
+    page = fetch_page url
+  end
+end
